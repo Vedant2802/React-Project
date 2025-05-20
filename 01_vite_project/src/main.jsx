7ab9090f79c67343028a1af35e196dc0e1ab5fa8
Vedant2802 <avedant981@gmail.com>
2025-05-20 23:27:50 +0530

    custom React

--- 01_vite_project/src/main.jsx
+++ 01_vite_project/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/* eslint-disable react-refresh/only-export-components */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// const myName = "Akshat Vedant";
+
+function MyApp() {
+  return <h1>Vedant</h1>;
+}
+// const customElement = React.createElement(
+//   "a",
+//   {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   "this is google.com"
+//   // myName
+//   // "Akshat Vedant"
+// );
+createRoot(document.getElementById("root"))
+  .render
+  // <StrictMode>
+  // <App />
+  // customElement
+  // <MyApp />
+  // Since at the end of the day the react components are functions itself you can write it like this , but this is not a good practice
+  // MyApp()
+  // </StrictMode>
+  ();
